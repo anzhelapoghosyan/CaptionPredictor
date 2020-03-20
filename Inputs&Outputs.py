04f3e26eaae5e48f490dad49253ea55f6f29ef73
Anzhela Poghosyan <43003610+anzhelapoghosyan@users.noreply.github.com>
2020-03-20 13:45:25 +0400

    Update Inputs&Outputs.py
    Created objects for picture

--- Inputs&Outputs.py
+++ Inputs&Outputs.py
@@ -12,19 +12,35 @@ p1_hashtags = 1
 p1_likes = 274
 p1_comments = 17
 
+p1 = {
+  #for input
+  "gender" : F,
+  "occasion" : "Event",
+  "type" : "Selfie",
+  "studyflex" = Y,
+  
+  #for output
+  "language" : "English",
+  "length" : 3,
+  "emojis" : 2,
+  "hashtags" : 1,
+  "likes" : 274,
+  "comments" : 17
+}
+
 #User inputs
 input_gender = input("Please choose your gender (F/M): ")
 input_occasion = input("Please specify the occasion (Event/Graduation/Lifestyle/Landscape): ")
 input_type = input("Please specify the the of pictures to analyze (Selfie/Taken by someone else): ")
 input_studyflex = input("Please specify whether study flex should be apparent in the posts (Yes/No): ")
 
-if (input_gender == p1_gender and input_occasion == p1_occasion and input_type == p1_type and input_studyflex == p1_studyflex):
-  print("The captions of pictures that fit your description are usually in the", p1_language, "language")
-  print("Have a length of ", p1_length, "words")
-  print(p1_emojis, "emojis")
-  print(p1_hashtags, "hashtags")
-  print("Prediced number of", p1_likes, "likes")
-  print("Prediced number of", p1_comments, "comments")
+if (input_gender == p1["gender"] and input_occasion == p1["occasion"] and input_type == p1["type"] and input_studyflex == p1["studyflex"]):
+  print("The captions of pictures that fit your description are usually in the", p1["language"], "language")
+  print("Have a length of ", p1["length"], "words")
+  print(p1["emojis"], "emojis")
+  print(p1["hashtags"], "hashtags")
+  print("Prediced number of", p1["likes"], "likes")
+  print("Prediced number of", p1["comments"], "comments")
 else:
   print("There are no similar pictures in my db")
   #ToDo ask the user ....
