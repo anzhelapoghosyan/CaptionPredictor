245416cddafc52e258ba980a8caea0b60648d9af
Anzhela Poghosyan <43003610+anzhelapoghosyan@users.noreply.github.com>
2020-03-25 20:57:00 +0400

    Update averages.py

--- averages.py
+++ averages.py
@@ -1,9 +1,6 @@
 sumOfLength = 0
 sumOfEmojis = 0
 sumOfHashtags = 0
-sumOfLikes = 0
-sumOfComments = 0
-sumOfFollowers = 0
 
 for item in data:
 	sumOfLength = sumOfLength + float(item["Length"])
@@ -14,21 +11,9 @@ for item in data:
 for item in data:
 	sumOfHashtags = sumOfHashtags + float(item["# of hashtags"])
 
-for item in data:
-	sumOfLikes = sumOfLikes + float(item["Likes"])
-
-for item in data:
-	sumOfComments = sumOfComments + float(item["Comments"])
-
-for item in data:
-	sumOfFollowers = sumOfFollowers + float(item["Followers"])
-  
 avgLength = sumOfLength/(len(data))
 avgEmojis = sumOfEmojis/(len(data))
 avgHashtags = sumOfHashtags/(len(data))
-avgLikes = sumOfLikes/(len(data))
-avgComments = sumOfComments/(len(data))
-avgFollowers = sumOfFollowers/(len(data))
 
 print("Average length of captions: ", int(avgLength), "words")
 print("And an average of: ")
